da2c9034c9005578448489237eba2a230efa5450
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2023-07-11 17:25:27 +0530

    Updated Solution - GfG to GitHub

--- Easy/First Repeating Element/first-repeating-element.cpp
+++ Easy/First Repeating Element/first-repeating-element.cpp
@@ -7,25 +7,29 @@ using namespace std;
 // } Driver Code Ends
 // User function template in C++
 
+// User function template in C++
+
 class Solution {
   public:
     // Function to return the position of the first repeating element.
     int firstRepeated(int arr[], int n)
     {
-        unordered_map<int,int>temp;
+        unordered_set<int>temp;
         vector<int>pusher;
         for(int i=n-1;i>=0;i--)
         {
-            temp[arr[i]]++;
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(temp[arr[i]]>1)
+            if(temp.find(arr[i])==temp.end())
             {
-                return i+1;
+                temp.insert(arr[i]);
+            }
+            else
+            {
+                pusher.push_back(i+1);
             }
         }
-        return -1;
+        if(pusher.empty())
+            return -1;
+        return pusher.back();
     }
 };
 
